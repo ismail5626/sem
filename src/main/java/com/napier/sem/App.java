68dd1a4e75cd86dd1541d596fc5280a4a2cb13f8
ismail5626 <40653006@live.napier.ac.uk>
2024-10-03 14:35:47 +0100

    Added "Welcome to my system" message

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,15 @@
 package com.napier.sem;
 
+import javax.xml.namespace.QName;
+
 public class App
 {
     public static void main(String[] args)
     {
+
         System.out.println("Boo yah!");
+        System.out.println("Welcome to my system!");
     }
+
+
 }
